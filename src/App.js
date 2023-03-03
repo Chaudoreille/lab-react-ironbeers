6147878c3097dc8f38f2c924ac24e90c0a4c6e31
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-03 17:58:27 +0100

    [IT 2] - done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Home from './components/pages/Home';
 import Beers from './components/pages/Beers';
 import CreateBeer from './components/pages/CreateBeer';
 import RandomBeer from './components/pages/RandomBeer';
+import Layout from './components/pages/Layout';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/beers" element={<Beers />}></Route>
-        <Route path="/random-beer" element={<RandomBeer />}></Route>
-        <Route path="/new-beer" element={<CreateBeer />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/beers" element={<Beers />}></Route>
+          <Route path="/random-beer" element={<RandomBeer />}></Route>
+          <Route path="/new-beer" element={<CreateBeer />}></Route>
+        </Route>
         <Route path='*' element={<h2>Error 404</h2>}></Route>
       </Routes>
     </div>
