05996e7cda92b9026ff7e8b184c828de8d33ee28
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-03 18:12:22 +0100

    [IT 3] - beers page - done

--- src/components/pages/Beers.jsx
+++ src/components/pages/Beers.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BeersImg from "../../assets/beers.png";
+import { Link } from "react-router-dom";
 
 const Beers = () => {
+  const [allBeers, setAllBeers] = useState();
+
+  useEffect(() => {
+    fetch('https://ih-beers-api2.herokuapp.com/beers')
+      .then(res => res.json())
+      .then(json => setAllBeers(json))
+      .catch(error => console.error(error));
+  }, []);
+
+  if (!allBeers) {
     return (
-        <div>
-            <img src={BeersImg} alt="beers" />
-        </div>
+      <div>
+        <img src={BeersImg} alt="beers" />
+        <p>Loading</p>
+      </div>
     );
+  }
+  return (
+    <div>
+      <img src={BeersImg} alt="beers" />
+      <ul>
+        {allBeers.map(beer => (
+          <li key={beer._id}>
+            <Link to={`/beers/${beer._id}`}></Link>
+            <img src={beer.image_url} alt="beer" />
+            <h2>{beer.name}</h2>
+            <p>{beer.tagline} </p>
+            <p>{beer.contributed_by} </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Beers;
