5665681c1d3b95d0923b0e322e9e54e8393d3995
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-06 10:15:14 +0100

    [IT 6] - create beer - done

--- src/components/pages/CreateBeer.jsx
+++ src/components/pages/CreateBeer.jsx
@@ -1,12 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import newBeerImg from "../../assets/new-beer.png";
 
 const CreateBeer = () => {
-    return (
+  const [fields, setFields] = useState({});
+  const [message, setMessage] = useState([]);
+
+  const updateField = (fieldName, value) => {
+    const fieldUpdate = { [fieldName]: value };
+
+    setFields(currentFields => ({ ...currentFields, ...fieldUpdate }));
+  };
+
+  const sendForm = (event) => {
+    event.preventDefault();
+
+    fetch('https://ih-beers-api2.herokuapp.com/beers/new', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(fields)
+    })
+      .then(res => res.json())
+      .then(data => {
+        setMessage(data.message);
+        setFields({});
+      })
+      .catch(error => console.error(error));
+  };
+
+  const saveField = (event) => updateField(event.currentTarget.name, event.currentTarget.value);
+
+  return (
+    <div>
+      <img src={newBeerImg} alt="new beer" />
+      <form>
         <div>
-            <img src={newBeerImg} alt="new beer" />
+          <span className="label">Name</span>
+          <input onChange={saveField} type="text" name="name" value={fields.name || ""} />
         </div>
-    );
+        <div>
+          <span className="label">Description</span>
+          <input onChange={saveField} type="text" name="description" value={fields.description || ""} />
+        </div>
+        <div>
+          <span className="label">Tagline</span>
+          <input onChange={saveField} type="text" name="tagline" value={fields.tagline || ""} />
+        </div>
+        <div>
+          <span className="label">First brewed</span>
+          <input onChange={saveField} type="date" name="first_brewed" value={fields.first_brewed || ""} />
+        </div>
+        <div>
+          <span className="label">Brewers tips</span>
+          <input onChange={saveField} type="text" name="brewers_tips" value={fields.brewers_tips || ""} />
+        </div>
+        <div>
+          <span className="label">Attenuation level</span>
+          <input onChange={saveField} type="number" name="attenuation_level" value={fields.attenuation_level || ""} />
+        </div>
+        <div>
+          <span className="label">Contributed by</span>
+          <input onChange={saveField} type="text" name="contributed_by" value={fields.contributed_by || ""} />
+        </div>
+        <button onClick={sendForm}>Create A Beer !</button>
+      </form>
+      <div id="message">
+        <p>{message}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CreateBeer;
