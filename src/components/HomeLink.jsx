972f2d41c6c8e23042c8649574d19f93b01d161a
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-06 13:26:39 +0100

    [BEAUTY] - home page details

--- src/components/HomeLink.jsx
+++ src/components/HomeLink.jsx
@@ -5,7 +5,7 @@ const HomeLink = ({ link, image, title, text }) => {
   return (
     <Link className="card text-decoration-none text-start" to={link}>
       <img className="card-img-top" src={image} alt="beers" />
-      <h3 className="card-title px-3 text-black fw-normal">{title}</h3>
+      <h3 className="card-title px-3 pt-3 text-black fw-normal">{title}</h3>
       <p className="card-text px-3 pb-3 text-secondary text-opacity-50">
         {text}
       </p>
