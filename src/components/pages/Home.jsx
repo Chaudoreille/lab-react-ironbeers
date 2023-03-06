f0764b3758593531b98fa045d3b5ef7789ba7df3
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-06 13:24:25 +0100

    [BEAUTY] - Home page done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import BeersImg from "../../assets/beers.png";
+import RandomBeerImg from "../../assets/random-beer.png";
+import newBeerImg from "../../assets/new-beer.png";
+import HomeLink from "../HomeLink";
 
 const Home = () => {
   return (
     <div>
-      <ul>
+      <ul className="d-flex flex-column align-items-center m-auto list-unstyled" style={{ maxWidth: "705px" }}>
         <li>
-          <Link to="/beers">Beers</Link>
+          <HomeLink
+            link="/beers"
+            image={BeersImg}
+            title="All Beers"
+            text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae totam cupiditate maxime animi nostrum, voluptate cum dignissimos culpa molestiae, quia sequi, impedit dolor architecto consectetur tenetur. Aut ad dolore inventore."
+          />
         </li>
         <li>
-          <Link to="/random-beer">Random Beer</Link>
+          <HomeLink
+            link="/random-beer"
+            image={RandomBeerImg}
+            title="Random Beer"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus minima, temporibus, cum laboriosam nisi cumque dolor sed atque maxime velit tempora eveniet nam aliquam explicabo sit odit debitis. Consequatur?"
+          />
         </li>
         <li>
-          <Link to="/new-beer">New Beer</Link>
+          <HomeLink
+            link="/new-beer"
+            image={newBeerImg}
+            title="New Beer"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque possimus blanditiis accusantium porro dolores temporibus totam incidunt! Totam distinctio, modi ratione suscipit, fugiat sapiente recusandae architecto rem voluptates incidunt voluptatem."
+          />
         </li>
       </ul>
     </div>
